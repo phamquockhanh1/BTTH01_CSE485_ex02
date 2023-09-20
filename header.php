49ccf71750b341c35c6bfe5cee0fc885a4de0b44
Khanh <banitthoi@gmail.com>
2023-09-20 21:16:12 +0700

    Thông điệp commit của bạn

---
+++ header.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        input{
+            border-radius: 2px 2px 2px 2px;
+            border: none;
+            height: 35px;
+}
+    button{
+        border: none;
+        width: 30%;
+        height: 35px;
+        padding-right: 3%;
+
+}
+body{
+    height: 100px;
+}
+h3 {
+    padding-top: 20px;
+}
+
+.left{
+    display: flex;
+}
+.right{
+    display: flex;
+    padding-top:20px ;
+}
+button{
+    border: 2px;
+}
+.header a{
+    color: black;
+    text-decoration: none;
+}
+.header{
+    width: 90%;
+    height: 90%;
+    display: flex;
+    justify-content: space-between;
+}
+.header img{
+    width: 100%;
+    height: 100%;
+}
+    </style>
+</head>
+<body>
+<div class="header">
+        <div class="left">
+        <div class="logo"><img src="img/log.png "alt=""  ></div>
+        <h3><a href=""> Trang Chủ</a><sp
+        
+        <h3> <a href="">Đăng Nhập</a> </h3>
+    </div>
+    <div class="right" style="padding-top:10px"><input type="text" placeholder="Nội Dung Cần Tìm">
+     <button>Tìm</button></div>
+        </div>
+    
+</body>
+</html>
