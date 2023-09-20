4c67913cfd97f8311fec8f20ac07dbd9609adc0a
KaTm <93910089+phamquockhanh1@users.noreply.github.com>
2023-09-20 20:34:35 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+    <div class="header">
+        <div class="left">
+        <div class="logo"><img src="img/log.png "alt="" style="width: 100%; height:40%;" ></div>
+        <h3><a href=""> Trang Chủ</a><sp
+        
+        <h3> <a href="">Đăng Nhập</a> </h3>
+    </div>
+    <div class="right" style="padding-top:10px"><input type="text" placeholder="Nội Dung Cần Tìm">
+     <button>Tìm</button></div>
+        </div>
+        <div class="mainimg">
+                <img src="img/backgrdead.jpg" alt="">
+        </div>
+        <div class="text"><h1 style="color: blue; text-align: center; " >Top Bài Hát Yêu Thích</h1></div>
+        <div class="loopcontent">
+            <div class="box1"><img src="" alt=""><h3><span></span></h3></div>
+            <div class="box2"><img src="" alt=""><h3><span></span></h3></div>
+            <div class="box3"><img src="" alt=""><h3><span></span></h3></div>
+            <div class="box4"><img src="" alt=""><h3><span></span></h3></div>
+            
+</div>
+    </div>
+    
+</body>
+</html>
